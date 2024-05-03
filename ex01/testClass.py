05b647f5c7a809b20d658fb5ea6e8516f5df48ad
Valentin Porceddu EI <porceddu.valentn@gmail.com>
2024-05-03 14:16:42 +0200

    ex01 find how do optional arguments for __init__

--- ex01/testClass.py
+++ ex01/testClass.py
@@ -1,10 +1,7 @@
 class test:
-	def __init__(self,arg1,arg2):
+	def __init__(self,arg1,arg2='other'):
 		self.a = arg1
-		if arg2:
-			self.b = arg2
-		else:
-			self.b = 0
+		self.b = arg2
 
 salut = test('e',2)
 beu = test('beuuuu')
