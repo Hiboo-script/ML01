60277812e5dfe512d8f5875d21ccdd9d34be52af
Valentin Porceddu EI <porceddu.valentn@gmail.com>
2024-05-04 13:34:23 +0200

    ex01 before crashtests errors instanciation management !

--- ex01/recipe.py
+++ ex01/recipe.py
@@ -8,25 +8,32 @@ class Recipe:
 		description=""):
 		try:
 			if not isinstance(name, str):
-				raise ValueError("argumentError : name of Recipe must be a str !")
+				raise ValueError("typeError : name of Recipe must be a <str> !")
 			elif not isinstance(cooking_lvl,int):
-				raise ValueError("argumentError : cooking_lvl of resipe must be an int !")
+				raise ValueError("typeError : cooking_lvl of resipe must be an <int> !")
 			elif cooking_lvl<1 and cooking_lvl>5:
 				raise Value_Error("rangeError : cooking_lvl of recipe must be between 1 and 5 inclusive !")
 			elif not isinstance(cooking_time, int):
-				raise valueError("argumentError : cooking_time must be an int !")
+				raise ValueError("typeError : cooking_time must be an <int> !")
 			elif cooking_time<0:
-				raise valueError("physicalError : cooking_time cannot be negative !")
-			elif ingredient
+				raise ValueError("physicalError : cooking_time cannot be negative !")
+			elif not isinstance(ingredients,list):
+				raise ValueError("typeError : ingredients must be a list !"))
+			elif not all(isinstance(ingredient, str) for ingredient in ingredients):
+				raise ValueError("typeError : all ingredients must be a <str> !")
+			elif not recipe_type in {"starter","lunch","dessert"}:
+				raise ValueError("valueError : recipe_type must be 'starter', 'lunch' or 'dessert' !")
+			elif not isinstance(description, str):
+				raise ValueError("typeError : description must be a <str> !")
 			else:
 				self.name = name
+				self.cooking_lvl = cooking_lvl
+				self.cooking_time = cooking_time
+				self.ingredients = ingredients
+				self.recipe_type = recipe_type
+				self.desscription = description
 		except ValueError as e:
 			print(e)
 			quit()
-		self.cooking_lvl = cooking_lvl
-		self.cooking_time = cooking_time
-		self.ingredients = ingredients
-		self.recipe_type = recipe_type
-		self.desscription = description
 
-a = Recipe(0,0,0,0,0)
+a = Recipe(0,0,0,0)
