de8cff23a357572128cc089f088906dce9dd4954
Valentin Porceddu EI <porceddu.valentn@gmail.com>
2024-05-04 13:48:03 +0200

    ex01 Error instantiation management are solved for Recipe class.

--- ex01/recipe.py
+++ ex01/recipe.py
@@ -11,17 +11,19 @@ class Recipe:
 				raise ValueError("typeError : name of Recipe must be a <str> !")
 			elif not isinstance(cooking_lvl,int):
 				raise ValueError("typeError : cooking_lvl of resipe must be an <int> !")
-			elif cooking_lvl<1 and cooking_lvl>5:
-				raise Value_Error("rangeError : cooking_lvl of recipe must be between 1 and 5 inclusive !")
+			elif cooking_lvl<1 or cooking_lvl>5:
+				raise ValueError("rangeError : cooking_lvl of recipe must be between 1 and 5 inclusive !")
 			elif not isinstance(cooking_time, int):
 				raise ValueError("typeError : cooking_time must be an <int> !")
 			elif cooking_time<0:
 				raise ValueError("physicalError : cooking_time cannot be negative !")
 			elif not isinstance(ingredients,list):
-				raise ValueError("typeError : ingredients must be a list !"))
+				raise ValueError("typeError : ingredients must be a list !")
 			elif not all(isinstance(ingredient, str) for ingredient in ingredients):
 				raise ValueError("typeError : all ingredients must be a <str> !")
-			elif not recipe_type in {"starter","lunch","dessert"}:
+			elif not isinstance(recipe_type,str):
+				raise ValueError("typeError : recipe type must be a <str> !")
+			elif not recipe_type in {"stiarter","lunch","dessert"}:
 				raise ValueError("valueError : recipe_type must be 'starter', 'lunch' or 'dessert' !")
 			elif not isinstance(description, str):
 				raise ValueError("typeError : description must be a <str> !")
@@ -36,4 +38,4 @@ class Recipe:
 			print(e)
 			quit()
 
-a = Recipe(0,0,0,0)
+a = Recipe("Bourguignon",3,4,["salut","cachuete"],"lunch","djfkdslshdsljks")
