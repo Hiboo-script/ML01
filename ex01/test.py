4d12a17f74b59015902dde7d06eb6e98c1cc9a4d
Valentin Porceddu EI <porceddu.valentn@gmail.com>
2024-05-04 22:04:12 +0200

    ex01 rm testClass.py; good importation of Recipe class !

--- ex01/test.py
+++ ex01/test.py
@@ -1,4 +1,4 @@
-import recipe
+from recipe import Recipe
 import unittest
 
 
@@ -6,47 +6,47 @@ class TestStringMethods(unittest.TestCase):
 
 	def test_nameRecipe(self):
 		with self.assertRaises(AttributeError):
-			yaourt = recipe.Recipe(78,3,20,["milk","berry"],"dessert","like in market")
+			yaourt = Recipe(78,3,20,["milk","berry"],"dessert","like in market")
 	
 	def test_typeLvl(self):	
 		with self.assertRaises(AttributeError):
-			yaourt = recipe.Recipe("yaourt","3",20,["milk","berry"],"dessert","like in market")
+			yaourt = Recipe("yaourt","3",20,["milk","berry"],"dessert","like in market")
 	
 	def test_indexLvlUp(self):	
 		with self.assertRaises(IndexError):
-			yaourt = recipe.Recipe("yaourt",6,20,["milk","berry"],"dessert","like in market")
+			yaourt = Recipe("yaourt",6,20,["milk","berry"],"dessert","like in market")
 	
 	def test_indexLvlDown(self):	
 		with self.assertRaises(IndexError):
-			yaourt = recipe.Recipe("yaourt",0,20,["milk","berry"],"dessert","like in market")
+			yaourt = Recipe("yaourt",0,20,["milk","berry"],"dessert","like in market")
 	
 	def test_typeTime(self):	
 		with self.assertRaises(AttributeError):
-			yaourt = recipe.Recipe("yaourt",3,"20",["milk","berry"],"dessert","like in market")
+			yaourt = Recipe("yaourt",3,"20",["milk","berry"],"dessert","like in market")
 	
 	def test_negativeTime(self):	
 		with self.assertRaises(ValueError):
-			yaourt = recipe.Recipe("yaourt",3,-20,["milk","berry"],"dessert","like in market")
+			yaourt = Recipe("yaourt",3,-20,["milk","berry"],"dessert","like in market")
 	
 	def test_typeIngredients(self):	
 		with self.assertRaises(AttributeError):
-			yaourt = recipe.Recipe("yaourt",3,20,"milk","dessert","like in market")
+			yaourt = Recipe("yaourt",3,20,"milk","dessert","like in market")
 	
 	def test_typeIngredient(self):	
 		with self.assertRaises(AttributeError):
-			yaourt = recipe.Recipe("yaourt",3,20,["milk","berry",45],"dessert","like in market")
+			yaourt = Recipe("yaourt",3,20,["milk","berry",45],"dessert","like in market")
 	
 	def test_cookingType(self):	
 		with self.assertRaises(ValueError):
-			yaourt = recipe.Recipe("yaourt",3,20,["milk","berry"],"dert","like in market")
+			yaourt = Recipe("yaourt",3,20,["milk","berry"],"dert","like in market")
 	
 	def test_typeCookingType(self):	
 		with self.assertRaises(AttributeError):
-			yaourt = recipe.Recipe("yaourt",3,20,["milk","berry"],89,"like in market")
+			yaourt = Recipe("yaourt",3,20,["milk","berry"],89,"like in market")
 	
 	def test_typeDescription(self):	
 		with self.assertRaises(AttributeError):
-			yaourt = recipe.Recipe("yaourt",3,20,["milk","berry"],"dessert",78)
+			yaourt = Recipe("yaourt",3,20,["milk","berry"],"dessert",78)
 
 	
 if __name__ == '__main__':
--- ex01/testClass.py
+++
@@ -1,9 +0,0 @@
-class test:
-	def __init__(self,arg1,arg2='other'):
-		self.a = arg1
-		self.b = arg2
-
-salut = test('e',2)
-beu = test('beuuuu')
-print(f'{salut.a},{salut.b}')
-print(f'{beu.a},{beu.b}')
