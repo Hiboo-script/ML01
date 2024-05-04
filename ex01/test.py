dee7ce34793770c130078dd382afab36848bea7e
Valentin Porceddu EI <porceddu.valentn@gmail.com>
2024-05-04 19:55:06 +0200

    ex01 unitaries tests for recipe __init__ exceptions are solved !

--- ex01/test.py
+++ ex01/test.py
@@ -1,29 +1,53 @@
 import recipe
 import unittest
 
-salade = recipe.Recipe("salad",1,10,["onion","oil","salad"],"starter")
-print(str(salade))
-
 
 class TestStringMethods(unittest.TestCase):
 
-	def test_upper(self):
-		self.assertEqual('foo'.upper(), 'FOO')
-
-	def test_isupper(self):
-		self.assertTrue('FOO'.isupper())
-		self.assertFalse('Foo'.isupper())
-
-	def test_split(self):
-		s = 'hello world'
-		self.assertEqual(s.split(), ['hello', 'world'])
-		# check that s.split fails when the separator is not a string
-		with self.assertRaises(TypeError):
-			s.split(2)
-	
 	def test_nameRecipe(self):
-		with self.assertRaises(ValueError):
+		with self.assertRaises(AttributeError):
 			yaourt = recipe.Recipe(78,3,20,["milk","berry"],"dessert","like in market")
+	
+	def test_typeLvl(self):	
+		with self.assertRaises(AttributeError):
+			yaourt = recipe.Recipe("yaourt","3",20,["milk","berry"],"dessert","like in market")
+	
+	def test_indexLvlUp(self):	
+		with self.assertRaises(IndexError):
+			yaourt = recipe.Recipe("yaourt",6,20,["milk","berry"],"dessert","like in market")
+	
+	def test_indexLvlDown(self):	
+		with self.assertRaises(IndexError):
+			yaourt = recipe.Recipe("yaourt",0,20,["milk","berry"],"dessert","like in market")
+	
+	def test_typeTime(self):	
+		with self.assertRaises(AttributeError):
+			yaourt = recipe.Recipe("yaourt",3,"20",["milk","berry"],"dessert","like in market")
+	
+	def test_negativeTime(self):	
+		with self.assertRaises(ValueError):
+			yaourt = recipe.Recipe("yaourt",3,-20,["milk","berry"],"dessert","like in market")
+	
+	def test_typeIngredients(self):	
+		with self.assertRaises(AttributeError):
+			yaourt = recipe.Recipe("yaourt",3,20,"milk","dessert","like in market")
+	
+	def test_typeIngredient(self):	
+		with self.assertRaises(AttributeError):
+			yaourt = recipe.Recipe("yaourt",3,20,["milk","berry",45],"dessert","like in market")
+	
+	def test_cookingType(self):	
+		with self.assertRaises(ValueError):
+			yaourt = recipe.Recipe("yaourt",3,20,["milk","berry"],"dert","like in market")
+	
+	def test_typeCookingType(self):	
+		with self.assertRaises(AttributeError):
+			yaourt = recipe.Recipe("yaourt",3,20,["milk","berry"],89,"like in market")
+	
+	def test_typeDescription(self):	
+		with self.assertRaises(AttributeError):
+			yaourt = recipe.Recipe("yaourt",3,20,["milk","berry"],"dessert",78)
 
+	
 if __name__ == '__main__':
     unittest.main()
