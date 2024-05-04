8da09ed224b2d76a98c69945ae3e9124393454f8
Valentin Porceddu EI <porceddu.valentn@gmail.com>
2024-05-04 19:21:12 +0200

    ex01 better exceptions utilisations in Recipe class !

--- ex01/recipe.py
+++ ex01/recipe.py
@@ -6,38 +6,37 @@ class Recipe:
 		cooking_lvl,cooking_time,
 		ingredients,recipe_type,
 		description=""):
-		try:
-			if not isinstance(name, str):
-				raise ValueError("typeError : name of Recipe must be a <str> !")
-			elif not isinstance(cooking_lvl,int):
-				raise ValueError("typeError : cooking_lvl of resipe must be an <int> !")
-			elif cooking_lvl<1 or cooking_lvl>5:
-				raise ValueError("rangeError : cooking_lvl of recipe must be between 1 and 5 inclusive !")
-			elif not isinstance(cooking_time, int):
-				raise ValueError("typeError : cooking_time must be an <int> !")
-			elif cooking_time<0:
-				raise ValueError("physicalError : cooking_time cannot be negative !")
-			elif not isinstance(ingredients,list):
-				raise ValueError("typeError : ingredients must be a list !")
-			elif not all(isinstance(ingredient, str) for ingredient in ingredients):
-				raise ValueError("typeError : all ingredients must be a <str> !")
-			elif not isinstance(recipe_type,str):
-				raise ValueError("typeError : recipe type must be a <str> !")
-			elif not recipe_type in {"starter","lunch","dessert"}:
-				raise ValueError("valueError : recipe_type must be 'starter', 'lunch' or 'dessert' !")
-			elif not isinstance(description, str):
-				raise ValueError("typeError : description must be a <str> !")
-			else:
-				self.name = name
-				self.cooking_lvl = cooking_lvl
-				self.cooking_time = cooking_time
-				self.ingredients = ingredients
-				self.recipe_type = recipe_type
-				self.desscription = description
-		except ValueError as e:
-			print(e)
-			quit()
+		
+		if not isinstance(name, str):
+			raise AttributeError("name of Recipe must be a <str> !")
+		elif not isinstance(cooking_lvl,int):
+			raise AttributeError("cooking_lvl of resipe must be an <int> !")
+		elif cooking_lvl<1 or cooking_lvl>5:
+			raise IndexError("cooking_lvl of recipe must be between 1 and 5 inclusive !")
+		elif not isinstance(cooking_time, int):
+			raise AttributeError("cooking_time must be an <int> !")
+		elif cooking_time<0:
+			raise ValueError("cooking_time cannot be negative !")
+		elif not isinstance(ingredients,list):
+			raise AttributeError("ingredients must be a list !")
+		elif not all(isinstance(ingredient, str) for ingredient in ingredients):
+			raise AttributeError("all ingredients must be a <str> !")
+		elif not isinstance(recipe_type,str):
+			raise AttributeError("recipe type must be a <str> !")
+		elif not recipe_type in {"starter","lunch","dessert"}:
+			raise ValueError("recipe_type must be 'starter', 'lunch' or 'dessert' !")
+		elif not isinstance(description, str):
+			raise AttributeError("description must be a <str> !")
+		else:
+			self.name = name
+			self.cooking_lvl = cooking_lvl
+			self.cooking_time = cooking_time
+			self.ingredients = ingredients
+			self.recipe_type = recipe_type
+			self.desscription = description
 
-		def __str__(self):
-			recipe_txt = f"recipe : {recipe.name}"
-			return recipe_txt
+	def __str__(self):
+		recipe_txt = f"recipe : {self.name}"
+		return recipe_txt
+
+
--- ex01/test.py
+++ ex01/test.py
@@ -1,3 +1,29 @@
 import recipe
+import unittest
 
 salade = recipe.Recipe("salad",1,10,["onion","oil","salad"],"starter")
+print(str(salade))
+
+
+class TestStringMethods(unittest.TestCase):
+
+	def test_upper(self):
+		self.assertEqual('foo'.upper(), 'FOO')
+
+	def test_isupper(self):
+		self.assertTrue('FOO'.isupper())
+		self.assertFalse('Foo'.isupper())
+
+	def test_split(self):
+		s = 'hello world'
+		self.assertEqual(s.split(), ['hello', 'world'])
+		# check that s.split fails when the separator is not a string
+		with self.assertRaises(TypeError):
+			s.split(2)
+	
+	def test_nameRecipe(self):
+		with self.assertRaises(ValueError):
+			yaourt = recipe.Recipe(78,3,20,["milk","berry"],"dessert","like in market")
+
+if __name__ == '__main__':
+    unittest.main()
