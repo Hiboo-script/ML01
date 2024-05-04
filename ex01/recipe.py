674eae71070a8218daf2261e0dc56d3d7252d1c2
Valentin Porceddu EI <porceddu.valentn@gmail.com>
2024-05-04 13:07:57 +0200

    ex01 error instanciation management in writing...

--- ex01/recipe.py
+++ ex01/recipe.py
@@ -9,6 +9,15 @@ class Recipe:
 		try:
 			if not isinstance(name, str):
 				raise ValueError("argumentError : name of Recipe must be a str !")
+			elif not isinstance(cooking_lvl,int):
+				raise ValueError("argumentError : cooking_lvl of resipe must be an int !")
+			elif cooking_lvl<1 and cooking_lvl>5:
+				raise Value_Error("rangeError : cooking_lvl of recipe must be between 1 and 5 inclusive !")
+			elif not isinstance(cooking_time, int):
+				raise valueError("argumentError : cooking_time must be an int !")
+			elif cooking_time<0:
+				raise valueError("physicalError : cooking_time cannot be negative !")
+			elif ingredient
 			else:
 				self.name = name
 		except ValueError as e:
